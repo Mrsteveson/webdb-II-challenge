8a277804e6485152e53e2da5b84a82eadef8d5ae
mrsteveson <patrick.steveson727@gmail.com>
2019-04-30 13:31:42 -0500

    project initialized

---
+++ zoo/zoo-router.js
@@ -0,0 +1,20 @@
+const knex = require('knex');
+const router = require('express').Router();
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lambda.db3'
+    },
+    useNullAsDefault: true,
+};
+  
+const db = knex(knexConfig);
+
+
+
+
+
+
+
+module.exports = router;
